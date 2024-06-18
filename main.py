c06d3a6319e72499d0e1882a9b51a4cceab68d68
afg360 <63133426+afg360@users.noreply.github.com>
2024-06-18 17:33:55 -0400

    Init git repo, fastapi project

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+
+
+"""
+Structure;
+/api/realtime/{agency}/{bus_num}&{direction}
+
+/api/data/{agency}/{bus_num}
+"""
+
+
+app = FastAPI()
+
+
+@app.get("/api/realtime/{agency}")
+def get_data_agency(agency: str):
+    return {"Hello": agency}
