939767bfb034c9d825c8ac329757575fccdbe79a
afg360 <63133426+afg360@users.noreply.github.com>
2024-10-06 11:32:46 -0400

    Added a start script to more easily start the backend app on the local ip address

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+#check if activated python env
+#modify the path as needed
+python="$PROJECTS/android-apps/bus2go/api/venv/bin/python"
+[ "$(which python)" != "$python" ] && source ./venv/bin/activate
+#small script to start the bus2go server for testing, at the ip address of the device, port 8000
+
+#ip="$(ip address | awk '/inet.*wlp.s./ {print $2}' | sed 's/\(.*\)\/.*/\1/')"
+ip="$(ip address | awk '/inet.*wlp.s./ {print $2}' | sed 's/\/.*//')"
+
+printf "Ip: %s\n" "$ip"
+uvicorn main:app --host $ip --port 8000
